7fc7ff795c59a92d6f507948ddae493792adbac1
Ramakrishna <rkdevalla@gmail.com>
2018-09-04 07:20:57 -0700

    commiting deleted file

---
+++ test.java
@@ -0,0 +1,23 @@
+public class Learning_Git{
+
+	public static void main (String args[]){
+		system.pot.println("Hello world");
+		system.out.println("second line");
+		System.out.println("Third Line");
+		system.out.println("fourth line");
+		2343243432
+			3243243244
+	}
+324324
+234
+324
+234
+234
+4
+234
+4
+23423
+423
+423
+4234
+}
