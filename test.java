50543363e543086487465bb34e9c1abb01040c03
Ramakrishna <rkdevalla@gmail.com>
2018-09-04 06:36:26 -0700

    check configs

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class Learning_Git{
 	public static void main (String args[]){
 		system.pot.println("Hello world");
 		system.out.println("second line");
+		System.out.println("Third Line");
 	}
 }
